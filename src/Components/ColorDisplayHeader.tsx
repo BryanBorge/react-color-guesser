d8c3f48aaee28976fd8511fcc9613971ac125c05
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-11-05 10:08:05 -0400

    Change drop down to chips for color mode select

--- src/Components/ColorDisplayHeader.tsx
+++ src/Components/ColorDisplayHeader.tsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
-import { Box, Grid, Stack, Typography, IconButton, Tooltip, useTheme } from "@mui/material";
-import MenuItem from "@mui/material/MenuItem";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { Stack, IconButton, useTheme, Chip } from "@mui/material";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import { HelpDialog } from "../HelpDialog";
 import { useColorGuesser } from "../ColorGuesser/useColorGuesser";
@@ -11,19 +7,26 @@ import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import { ColorMode, useGameContext } from "../Context/GameContext";
 import { ActionType } from "../Context/ActionTypes";
 
+type ColorModeChip = {
+  type: ColorMode;
+};
+
+const ColorModes = [{ type: ColorMode.HEX }, { type: ColorMode.RGB }];
+
 export const ColorDisplayHeader = () => {
   const {
     store: { colorMode },
     dispatch,
   } = useGameContext();
   const [helpDialogOpen, setHelpDialogOpen] = useState<boolean>(false);
+  const [activeChip, setActiveChip] = React.useState<ColorMode>(colorMode);
   const { RestartGame } = useColorGuesser();
 
   const theme = useTheme();
 
-  const handleChange = (event: SelectChangeEvent) => {
-    const mode: ColorMode = event.target.value as ColorMode;
-    dispatch({ type: ActionType.SET_COLOR_MODE, payload: mode });
+  const handleClick = (colorMode: ColorModeChip) => {
+    setActiveChip(colorMode.type);
+    dispatch({ type: ActionType.SET_COLOR_MODE, payload: colorMode.type });
   };
 
   return (
@@ -34,16 +37,17 @@ export const ColorDisplayHeader = () => {
         direction="row"
         alignItems="center"
         mx={theme.spacing(1)}>
-        <FormControl sx={{ minWidth: 120 }} size="small">
-          <Select
-            value={colorMode}
-            onChange={handleChange}
-            displayEmpty
-            inputProps={{ "aria-label": "Without label" }}>
-            <MenuItem value={ColorMode.HEX}>Hex</MenuItem>
-            <MenuItem value={ColorMode.RGB}>Rgb</MenuItem>
-          </Select>
-        </FormControl>
+        <Stack direction="row" spacing={1}>
+          {ColorModes.map(colorMode => {
+            return (
+              <Chip
+                variant={activeChip === colorMode.type ? "filled" : "outlined"}
+                label={colorMode.type}
+                onClick={() => handleClick(colorMode)}
+              />
+            );
+          })}
+        </Stack>
         <Stack display="flex" justifyContent="flex-end" direction="row">
           <IconButton onClick={() => setHelpDialogOpen(true)} sx={{ marginTop: "8px", marginBottom: "8px" }}>
             <HelpOutlineIcon />
